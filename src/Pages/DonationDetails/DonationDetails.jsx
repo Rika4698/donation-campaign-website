37502c1602dc19ba31341758b594112ab2e97b63
Rika4698 <rikasharmin181@gmail.com>
2023-09-26 05:06:40 +0600

    show a toast/ sweet alert

--- src/Pages/DonationDetails/DonationDetails.jsx
+++ src/Pages/DonationDetails/DonationDetails.jsx
@@ -1,22 +1,54 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+
+import swal from "sweetalert";
 
 
 const DonationDetails = ({ card }) => {
     const { id, picture, title, text_button_bg, price, description } = card || {};
 
+    const handleAddToDonate = () => {
+        const addedDonateArray = [];
+        const donateItems = JSON.parse(localStorage.getItem("donate"));
+        if (!donateItems) {
+            addedDonateArray.push(card);
+            localStorage.setItem("donate", JSON.stringify( addedDonateArray));
+            swal("Good job!", "Donate added successfully in donation section.", "success");
+        }
+        else {
+            const isExits = donateItems.find((card) => card.id === id);
+            console.log(id,card.id);
+            
+            if (!isExits) {
+      
+                addedDonateArray.push(...donateItems, card);
+              localStorage.setItem("donate", JSON.stringify(addedDonateArray));
+              swal("Good job!", "Donate added successfully in donation section.", "success");
+             
+            } else {
+              swal("Sorry!", "Already added this card !", "error");
+            }
+      
+      
+          }
+      
+    };
+
     return (
         <div>
-            <div className="relative">
+            <div className="relative ">
                 <div className="flex justify-center items-center ">
-                    <img src={picture} alt="" className=" bg-cover   rounded-3xl mt-12 w-55 h-18     lg:w-[80%] h-[540px]  " />
+                    <img src={picture} alt="" className=" bg-cover   rounded-3xl mt-12 w-auto h-18     lg:w-10/12 h-[540px]  " />
                 </div>
-                <div className="absolute bottom-0 left-32 right-32  rounded-b-3xl bg-black bg-opacity-50 p-8 mx-2  transition-opacity  opacity-100 ">
+                <div className=" absolute bottom-0 rounded-b-3xl bg-black bg-opacity-50 p-8  transition-opacity  opacity-100 
+                 left-0 right-0 mx-0  lg:ml-28 mr-28">
+                 
+                  
 
-                    <button className="text-xl font-semibold text-white px-4 py-2 rounded " style={{ backgroundColor: text_button_bg }}>Donate ${price}</button>
+                    <button onClick={handleAddToDonate} className="text-xl font-semibold text-white px-4 py-2 rounded " style={{ backgroundColor: text_button_bg }}>Donate ${price}</button>
                 </div>
             </div>
-            <h1 className="text-4xl font-bold mx-32 my-10" style={{ color: text_button_bg }} >{title}</h1>
-            <p className="text-base font-normal text-[#0b0b0bb2] mx-32 mb-16">{description}</p>
+            <h1 className="text-4xl font-bold ml-12  my-10 md:mx-32   lg:mx-32  " style={{ color: text_button_bg }} >{title}</h1>
+            <p className="text-base font-normal text-[#0b0b0bb2] mx-12 mb-16 md:mx-32  lg:mx-32">{description}</p>
 
         </div>
 
