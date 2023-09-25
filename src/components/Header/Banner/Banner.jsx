3e11e8f17949012e24e2bf36f4d8217010ef2786
Rika4698 <rikasharmin181@gmail.com>
2023-09-25 10:35:01 +0600

    create banner section covering with bg-image and title,secrch bar

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -2,9 +2,27 @@
 
 const Banner = () => {
     return (
-        <div className="h-[70vh] w-100%  bg-[url('https://i.ibb.co/TghQnFJ/people-meeting-community-center.jpg')] bg-cover bg-center ">
-            <div className="h-[70vh] w-100% flex  justify-center items-center bg-white/30 backdrop-opacity-95"></div>
-            
+        <div className=" ">
+            <div className="h-[80vh] w-100%  bg-[url('https://i.ibb.co/TghQnFJ/people-meeting-community-center.jpg')] bg-cover bg-center ">
+                <div className="h-[80vh] w-100% flex  justify-center items-center bg-white/90 backdrop-brightness-75">
+                    <div className="grid ">
+                        <div className="font-bold text-3xl text-[#0B0B0B] text-center lg:text-5xl"> <h1>I Grow By Helping People In Need</h1></div>
+                        <div className="join justify-center items-center mt-10 w-auto ">
+                            <div>
+                                <div>
+                                    <input className="input input-bordered join-item lg: w-80" placeholder="Search here...." />
+                                </div>
+                            </div>
+
+                            <div className="">
+
+                                <button className="btn join-item  capitalize text-white text-sm bg-[#FF444A] font-semibold">Search</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
